96e606f38918a46fd16e8ba2f1d3c00fe13060e3
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-04-22 15:44:16 -0400

    Add files via upload

---
+++ src/VariablesAndTypes.java
@@ -0,0 +1,32 @@
+
+public class VariablesAndTypes {
+
+	public static void main(String[] args) {
+		//int examples
+		int score; //declare
+		score = 0; //initialize
+		
+		int x = -7; //declare and initialize
+		
+		//double example
+		double taxRate = 0.06;
+		
+		//boolean examples
+		boolean gameOver = true;
+		boolean loggedIn = false;
+		
+		//character examples
+		char letter = 'A';
+		char symbol = '?';
+		
+		//Output variables
+		System.out.println("Score: " + score);
+		System.out.println("x: " + x);
+		System.out.println("taxRate: " + taxRate);
+		System.out.println("gameOver: " + gameOver);
+		System.out.println("loggedIn: " + loggedIn);
+		System.out.println("letter: " + letter);
+		System.out.println("symbol: " + symbol);
+	}
+
+}
